aa58eeabf649d4e4165dbb972079eaefa7fcbd67
David Sena Oliveira <sena.ufc@gmail.com>
2024-03-11 15:38:24 +0000

    tko 0.2.2

--- 01_carro/draft.ts
+++ 01_carro/draft.ts
@@ -1,54 +1,27 @@
 
-// Nesse rascunho esta faltando a parte de dirigir o carro
-
-
-class Car{ //atributos
+class Car{
     pass: number; // Passageiros
     passMax: number; // limite de Passageiros
     gas: number; // tanque
     gasMax: number; // limite do tanque
     km: number; // quantidade de quilometragem
 
-    constructor() { //métodos
-        this.pass = 0;
-        this.passMax = 2;
-        this.gas = 0;
-        this.gasMax = 100;
-        this.km = 0;
+    constructor() {
     }
 
     enter(): void {
-        //clausulas de guarda
-        if (this.pass == this.passMax) {
-            console.log("fail: limite de pessoas atingido");
-            return;
-        }
-        this.pass += 1;
     }
 
     leave(): void {
-        if (this.pass == 0) {
-            console.log("fail: nao ha ninguem no carro");
-            return;
-        }
-        this.pass -= 1;
-    }
-
-    drive(dist: number): void {
-        if (this.pass == 0) {
-            console.log("fail: nao ha ninguem no carro");
-            return;
-        }
-        this.gas -= dist;
-        this.km += dist;
     }
 
     fuel(gas: number): void {
-        this.gas += gas;
     }
     
+    drive (km: number): void {
+    }
+
     toString(): string {
-        return `pass: ${this.pass}, gas: ${this.gas}, km: ${this.km}`;
     }
 };
 
@@ -69,8 +42,8 @@ function main() {
         if      (args[0] === "show")  { write(car.toString());          }
         else if (args[0] === "enter") { car.enter();                    }
         else if (args[0] === "leave") { car.leave();                    }
-        else if (args[0] === "drive") { car.drive(+args[1])}
         else if (args[0] === "fuel")  { car.fuel(+args[1]);             }
+        else if (args[0] === "drive") { car.drive(+args[1]);            }
         else if (args[0] === "end")   { break;                          }
         else                          { write("fail: comando invalido");}
     }
